a2667b0efc05d8065ce022a6ef3e37d936d06da1
Gustavo Batistic Ribeiro <gustavo.batistic@gmail.com>
2016-09-01 12:18:14 -0300

    Início do projeto

---
+++ ATV.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef struct jogador{
+
+};
+
+int cadastrar();
+int editar();
+int lista_jogadores();
+int estatisticas_jogador();
+int estatisticas_grupo();
+int inserir_estatisticas();
+int editar_estatisticas();
+int media();
+int pontuacao();
+int ranking();
+int melhor_escalacao();
+int grupo_notas();
+int grupo_GeF();
+int grupo_PeA();
+int grupo_Des();
+int grupo_Def();
+int grupo_FeP();
+int grupo_Imp();
+int jogador_dados();
+int jogador_GeF();
+int jogador_PeA();
+int jogador_des();
+int jogador_def();
+int jogador_FeP();
+int jogador_imp();
+int jogador_notas();
+
+int main()
+{
+
+    return 0;
+}
