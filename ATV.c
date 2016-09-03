81da7686c4e11bcf2e699bd24551ceb8bf580537
Gustavo Batistic Ribeiro <gustavo.batistic@gmail.com>
2016-09-03 15:51:00 -0300

    Declarei as structs e comentei o que já dava

--- ATV.c
+++ ATV.c
@@ -1,10 +1,65 @@
+/*
+ *                         A.T.V. - ASSISTENTE TÉCNICO VIRTUAL
+ *
+ *    Software desenvolvido para auxiliar equipes técnicas de times de futebol de campo
+ *
+ * Desenvolvido por:
+ *   Edson da Costa Vitor Junior
+ *   Guilherme Atihe de Oliveira
+ *   Gustavo Batistic Ribeiro
+ *   Pedro Henrique Maldonado Truzzi
+ *
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
-typedef struct jogador{
+/*    Estrutura onde os dados de um jogador são armazenados    */
 
-};
+typedef struct {
+    // Dados pessoais
+    char nome[100]; int idade; float altura; float peso; int pe; int uniforme; int posicao[18];
+    // Estatísticas em jogo
+    float notas[100];         float media_notas;
+    int passesC[100];         float media_passesC;
+    int passesE[100];         float media_passesE;
+    int finalizacoesC[100];   float media_finalizacoesC;
+    int finalizacoesE[100];   float media_finalizacoesE;
+    int desarmes[100];        float media_desarmes;
+    int gols[100];            float media_gols;
+    int faltasC[100];         float media_faltasC;
+    int faltasS[100];         float media_faltasS;
+    int imped[100];           float media_imped;
+    int assistG[100];         float media_assistG;
+    int assistF[100];         float media_assistF;
+    int penaltisC[100];       float media_penaltisC;
+    int penaltisS[100];       float media_penaltisS;
+    int penaltisP[100];       float media_penaltisP;
+}jogador;
+
+/*    Estrutura onde os dados de um goleiro são armazenados    */
+
+typedef struct {
+    // Dados pessoais
+    char nome[100]; int idade; float altura; float peso; int pe; int uniforme;
+    // Estatísticas em jogo
+    float notas[100];         float media_notas;
+    int passesC[100];         float media_passesC;
+    int passesE[100];         float media_passesE;
+    int defesas[100];         float media_defesas;
+    int golsS[100];           float media_golsS;
+    int golsC[100];           float media_golsC;
+    int faltasC[100];         float media_faltasC;
+    int faltasS[100];         float media_faltasS;
+    int imped[100];           float media_imped;
+    int assistG[100];         float media_assistG;
+    int assistF[100];         float media_assistF;
+    int penaltisC[100];       float media_penaltisC;
+    int penaltisS[100];       float media_penaltisS;
+    int penaltisP[100];       float media_penaltisP;
+}goleiro;
+
+/*    Protótipos das funções utilizadas    */
 
 int cadastrar();
 int editar();
@@ -13,8 +68,8 @@ int estatisticas_jogador();
 int estatisticas_grupo();
 int inserir_estatisticas();
 int editar_estatisticas();
-int media();
-int pontuacao();
+float media(float, int);  // função para calcular media
+float pontuacao(char *); // função para calcular pontuação do jogador
 int ranking();
 int melhor_escalacao();
 int grupo_notas();
@@ -32,6 +87,8 @@ int jogador_def();
 int jogador_FeP();
 int jogador_imp();
 int jogador_notas();
+
+/* Função principal */
 
 int main()
 {
